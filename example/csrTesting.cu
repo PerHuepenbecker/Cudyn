6dc07e7e1e1fc514ef4482faf207286a8678991a
PerHuepenbecker <perhuepenbecker@googlemail.com>
2025-05-06 15:26:10 +0000

    work on testing program

--- example/csrTesting.cu
+++ example/csrTesting.cu
@@ -36,60 +36,19 @@ std::vector<T> generate_multiplication_vector(size_t problem_size, T baseValue =
     return result;
 }
 
-template <typename T>
-struct DeviceDataCSR {
-    T* csr_matrix_data_d = nullptr;
-    size_t* csr_matrix_row_ptrs_d = nullptr;
-    size_t* csr_matrix_col_indices_d = nullptr;
-    size_t rows = 0;
-    size_t columns = 0;
-
-    DeviceDataCSR(const CSRMatrix<T>& csrMatrix);
-};
-
-template <typename T>
-DeviceDataCSR<T>::DeviceDataCSR(const CSRMatrix<T>& csrMatrix){
-    auto data = csrMatrix.get_data();
-    auto column_pointers = csrMatrix.get_col_indices();
-    auto row_pointers = csrMatrix.get_row_ptrs();
-
-    cudaMalloc(&csr_matrix_data_d, data.size() * sizeof(T));
-    cudaMalloc(&csr_matrix_col_indices_d, column_pointers.size() * sizeof(size_t));
-    cudaMalloc(&csr_matrix_row_ptrs_d, row_pointers.size() * sizeof(size_t));
     
-}
-
-template<typename T>
-struct DeviceDataSpMV{
-    DeviceDataCSR<T> csrData;
-    T* multiplicationVector = nullptr;
-    T* result = nullptr;
-};
-
-template<typename T>
-void allocateDeviceMemory(DeviceDataSpMV<T> deviceData){
-
-}
-
-template<typename T>
-void prepareCSRDeviceArrays(){}
-    
-
-
 int main(int argc, char** argv) {
     if(argc != 2){
         std::cerr << "Missing argument: filename" << std::endl;
         std::cerr << "Usage: <program name> <filename>" << std::endl;
         std::cerr << "Exiting.." << std::endl;
         exit(1);
-    }    
+    };
 
     // Extracting the filename from program arguments
     std::string filename = argv[1];
 
     auto fileDataType = MatrixMarketCSRParserBase::peekHeader(filename);
-
-
 
     if(fileDataType == MatrixMarketHeaderTypes::DataType::REAL){
 
@@ -110,9 +69,5 @@ int main(int argc, char** argv) {
 
 
         }
-    
-    
-
-
 
 }
