5d5eeae75a43a88267b91905f5635d9c15288b47
PerHuepenbecker <perhuepenbecker@googlemail.com>
2025-05-08 15:44:30 +0000

    implemented policy based approach for generic kernel specification

--- include/cudyn/scheduler.cuh
+++ include/cudyn/scheduler.cuh
@@ -7,6 +7,25 @@
 
 namespace Cudyn::Scheduler{
 
+
+    struct StandardScheduler {
+        template <typename Functor>
+        static void launch(GridConfiguration::KernelConfig config, Functor f) {
+            genericIrregularKernel<<<config.grid_dimensions, config.block_dimensions>>>(
+                config.total_tasks, config.grid_dimensions, f);
+        }
+    };
+
+
+    struct ReducedAtomicScheduler {
+        template <typename Functor>
+        static void launch(GridConfiguration::KernelConfig config, Functor f) {
+            genericIrregularKernelLowAtomics<<<config.grid_dimensions, config.block_dimensions>>>(
+                config.total_tasks, config.grid_dimensions, f);
+        }
+    };
+
+
     enum class KernelType{STANDARD, REDUCED_ATOMICS, BATCH};
 
     template <typename TaskFunctor>
@@ -60,64 +79,6 @@ namespace Cudyn::Scheduler{
         }
     }
 
-    template <typename TaskFunctor>
-    __global__ void genericIrregularKernelBatch(uint64_t total_tasks, uint64_t count_blocks, TaskFunctor f){
-
-        //block wide counter for dynamic task assignment based on the index of the data thats up for processing
-        __shared__ uint64_t counter;
-
-        //central initialization of the counter variable by the first thread of the block
-        if(threadIdx.x == 0) counter = 0;
-        __syncthreads();
-
-        // naive task splitting of total task count by the number of blocks used 
-        
-        uint64_t base_tasks = total_tasks / count_blocks;
-        uint64_t remainder = total_tasks % count_blocks;
-
-        // Calculate how many extra tasks this block gets
-        uint64_t block_tasks = base_tasks + (blockIdx.x < remainder);
-
-        // Compute the starting index for this block
-        uint64_t block_start_index = blockIdx.x * base_tasks + min((uint64_t)blockIdx.x, remainder);
-
-        //block_tasks += additional_task;
-
-        // Initialization of the inter block synchronization variable 
-
-        //uint64_t block_start_index = blockIdx.x * block_tasks + min((unsigned long) blockIdx.x, total_tasks % count_blocks);
-
-        // Definition of the variable t which serves for intra block synchronization
-
-        // Initial assignment of tasks to all threads to avoid synchronization issues right from the start by running directly into the atomic counter. 
-        uint64_t thread_id = threadIdx.x;
-        uint64_t num_threads = blockDim.x;
-
-        if(thread_id < block_tasks){
-            f(block_start_index+thread_id);
-        }
-
-        if(threadIdx.x == 0){
-            counter = min(num_threads, block_tasks);
-        }
-        __syncthreads();
-
-    
-        // Task distribution and frequent update of the intra block synchronization update variable t
-        const int chunk_size = 8;
-        while (true) {
-        uint64_t t = atomicAdd((unsigned long long*)&counter, chunk_size);
-            if (t >= block_tasks) break;
-
-            #pragma unroll
-            for (int i = 0; i < chunk_size; ++i) {
-                if (t + i >= block_tasks) break;
-                f(block_start_index + t + i);
-    }
-}
-
-        __syncthreads();
-    }
 
     template <typename TaskFunctor>
     __global__ void genericIrregularKernel(uint64_t total_tasks, uint64_t count_blocks, TaskFunctor f){
@@ -175,8 +136,33 @@ namespace Cudyn::Scheduler{
 };
 
 namespace Cudyn::Launcher{
+
+
+    template <typename SchedulingPolicy, typename TaskFunctor>
+    __host__ void launch(GridConfiguration::KernelConfig config, TaskFunctor f, int deviceId = 0) {
+        if (!GridConfiguration::Details::validatGridConfigurationForDevice(config, deviceId)) {
+            std::cerr << "Invalid Grid configuration for device" << std::endl;
+            std::exit(EXIT_FAILURE);
+        }
+
+        cudaEvent_t start, stop;
+        cudaEventCreate(&start);
+        cudaEventCreate(&stop);
+        cudaEventRecord(start);
+
+        SchedulingPolicy::template launch(config, f);
+
+        cudaEventRecord(stop);
+        cudaEventSynchronize(stop);
+
+        float milliseconds = 0;
+        cudaEventElapsedTime(&milliseconds, start, stop);
+        std::cout << "Kernel execution time (" << typeid(SchedulingPolicy).name() << "): "
+                  << milliseconds << " ms" << std::endl;
+    }
+
     template <typename TaskFunctor>
-    __host__ void launch(GridConfiguration::KernelConfig kernelConfig, TaskFunctor f, Scheduler::KernelType type = Scheduler::KernelType::STANDARD, int deviceId = 0){
+    __host__ void launch_old(GridConfiguration::KernelConfig kernelConfig, TaskFunctor f, Scheduler::KernelType type = Scheduler::KernelType::STANDARD, int deviceId = 0){
 
         // TODO: Integrate GridConfigruationChecking functionality
 
